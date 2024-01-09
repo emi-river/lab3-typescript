1f44a3cf9e3e7fb8fd1529893bb1ba3f3e02320e
emi-river <emi.riverdi@gmail.com>
2024-01-09 13:30:56 +0100

    Add new test

---
+++ frontend/cypress/e2e/actor.feature
@@ -0,0 +1,8 @@
+Feature: Användaren går till sidan med skådespelare.
+
+Det ska finnas en knapp som går till en sida med en lista på skådespelare
+
+Scenario: Användaren hamnar på sidan med skådespelare.
+  Given Användaren är på hemsidan och det finns en knapp "Actors".
+  When Användaren klickar på knappen.
+  Then Användaren hamnar på en sida med en lista på skådespelare.
---
+++ frontend/cypress/e2e/actor.ts
@@ -0,0 +1,14 @@
+import { When, Then, Given} from '@badeball/cypress-cucumber-preprocessor'
+const text = 'Actors'
+
+Given('Användaren är på hemsidan och det finns en knapp "Actors".', () => {
+  cy.visit("http://localhost:5173/")
+})
+
+When('Användaren klickar på knappen.', () => {
+  cy.get('nav').find('#actors').click()
+})
+
+Then('Användaren hamnar på en sida med en lista på skådespelare.', () => {
+cy.get('h2').should('have.text', text)
+})
