f78b38b0648f2d8458b09c87379a905e829cd21f
emi-river <emi.riverdi@gmail.com>
2024-01-14 16:26:34 +0100

    WIP

--- frontend/cypress/component/randomButton.cy.tsx
+++ frontend/cypress/component/randomButton.cy.tsx
@@ -3,11 +3,11 @@ const click = '#button'
 const container = '.randomMovie'
 const textElement = 'h3'
 const repeat = 3
-
+//Works locally in cypress but not in github actions?
 describe('randomButton.cy.jsx', () => {
   it('Get 3 random movies', () => {
     cy.mount(<Random />)
-    cy.wait(5000)
+    cy.wait(2000)
     for (let index = 0; index < repeat; index++) {
       cy.get(click).click()
       cy.get(container)
