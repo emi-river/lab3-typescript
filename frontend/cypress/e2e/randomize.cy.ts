8abd7ffec7ba0edead35ce28c4778121636ab4ff
emi-river <emi.riverdi@gmail.com>
2024-01-14 17:58:50 +0100

    WIP

--- frontend/cypress/e2e/randomize.cy.ts
+++ frontend/cypress/e2e/randomize.cy.ts
@@ -2,6 +2,7 @@ describe('Get a random movie', () => {
   it('visits my site to get a random movie', () => {
     cy.visit('http://localhost:5173/')
     cy.get('nav').find('#randomMovie').click()
+    cy.wait(2000)
     cy.get('button').click()
     cy.get('h3')
       .invoke('text')
