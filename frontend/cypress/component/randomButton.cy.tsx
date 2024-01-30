e5926f93656fa5de70c553d0283e42d14008e00f
emi-river <emi.riverdi@gmail.com>
2024-01-30 21:43:33 +0100

    WIP

--- frontend/cypress/component/randomButton.cy.tsx
+++ frontend/cypress/component/randomButton.cy.tsx
@@ -3,9 +3,55 @@ const click = '#randomButton'
 const container = '.randomMovie'
 const textElement = 'h3'
 const repeat = 3
-//Works locally in cypress but not in github actions?
+
 describe('randomButton.cy.jsx', () => {
   it('Get 3 random movies', () => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/movies'
+      },
+      {
+        body: [
+          {
+            name: 'Shrek'
+          },
+          {
+            name: 'Coco'
+          },
+          {
+            name: 'Spider-Man'
+          },
+          {
+            name: 'Rat Race'
+          },
+          {
+            name: 'The Lion King'
+          },
+          {
+            name: 'Night at the Museum'
+          },
+          {
+            name: 'The Incredibles'
+          },
+          {
+            name: 'Footloose'
+          },
+          {
+            name: 'Elemental'
+          },
+          {
+            name: 'Luca'
+          },
+          {
+            name: 'Encanto'
+          },
+          {
+            name: 'Free Guy'
+          }
+        ]
+      }
+    )
     cy.mount(<Random />)
     cy.wait(2000)
     for (let index = 0; index < repeat; index++) {
